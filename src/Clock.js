536afd48dfcf00e81159bcf2fc38a05868d1fb8d
Yashwanth-Ramanjineya <yashwanth.dr113@gmail.com>
2020-10-04 10:12:51 +0530

    Clock Compoennt

---
+++ src/Clock.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import myPhoto from "../src/imagesFolder/logo192.png";
+import "./App.css";
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     date: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    )
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  // componentWillUnmount() {
+  //   console.log("cOMPOEE EILL Unmount")
+  //   clearInterval(this.timerID)
+  // }
+
+  stopTimer = () => {
+    clearInterval(this.timerID)
+  }
+
+
+  render() {
+    console.log(this.state.date)
+    return (
+      <div className="App">
+        <h1>Hello World! Here is my Clock {this.state.date.toLocaleTimeString()}</h1>
+        <h2>It is </h2>
+        <button onClick={this.stopTimer}>Stop Timer</button>
+      </div>
+    );
+  }
+}
+
+export default Clock;
