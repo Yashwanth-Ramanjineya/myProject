713b8ffbc55b952498d5cbef0b04e36c578a6281
prathap <prathap@prathaps-MacBook-Air.local>
2020-10-06 20:17:25 +0530

    sunithaclock

---
+++ src/SunithaClock.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+class SunithaClock extends Component{
+    constructor(props){
+     super(props); 
+         this.state= {
+          date:new Date() 
+        }
+    }
+  render(){
+      
+        return(
+           <div ClassName="App">
+           <h1>Hello World!This  is SunithaClock</h1>
+           <h2>It is{this.state.date}</h2>
+           </div>
+        );
+    }
+   
+    
+}
+export default SunithaClock;
