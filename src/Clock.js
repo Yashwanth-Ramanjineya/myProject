14f589e14fddfbecc505ffadbb14e182e294c358
keerthi426 <keerthikompalli425@gmail.com>
2020-10-09 11:31:50 +0530

    closk

--- src/Clock.js
+++ src/Clock.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import myPhoto from "../src/imagesFolder/logo192.png";
 //import Bhaskarclock from './Bhaskarclock';
-import My2ndComponent from './My2ndComponent.js';
+import Keerthi from './Keerthi.js';
 import "./App.css";
 
 class Clock extends Component {
@@ -39,7 +39,7 @@ class Clock extends Component {
       <div className="App">
         <h1>Hello World! Here is my Clock {this.state.date.toLocaleTimeString()}</h1>
         <h2>It is </h2>
-        <My2ndComponent mydata="thisnis my parent class" mydata1="hello"/>
+        <Keerthi />
         <button onClick={this.stopTimer}>Stop Timer</button>
       </div>
     );
