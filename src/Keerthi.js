171d71b207152cfcfb464f4ba8bf90cfece1650d
keerthi426 <keerthikompalli425@gmail.com>
2020-10-12 23:36:48 +0530

    changes

--- src/Keerthi.js
+++ src/Keerthi.js
@@ -1,117 +1,87 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import My2ndComponent from "./My2ndComponent";
 import Date2 from "./Date2";
 
-<<<<<<< HEAD
-class Keerthi extends Component{
-    constructor(props){
-        super(props);
-            this.state={
-                date: new Date(),
-    
-
-            };
-        
-    }
-    componentDidMount(){
-        this.timerID=setInterval(
-            () =>this.tick(),1000
-        )
-    }
-    tick = () => {
-        this.setState({date: new Date()})
-      }
-      stopTimer = () => {
-        clearInterval(this.timerID)
-      }
-    render(){
-        console.log(this.state.date.toLocaleTimeString)
-    
-        return(
-            <div className="App">
-                <h1>This is Keerthi Component</h1>
-            
-                     <h1>Hi Here is My Clock {this.state.date.toLocaleTimeString()}</h1>
-                        <h2>here my list</h2>
-                       
-                        <My2ndComponent  mydata="thisnis my parent class" mydata1="hello" />
-                        <Date2 />
-=======
 class Keerthi extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      load: false,
-      listData: {
-        data: [
-          { name: "apple", address: "Pune", validity: "3 years" },
-          { name: "banana", address: "Bnagalore", validity: "2 years" },
-          { name: "hello", address: "Delhi", validity: "120 years" },
-          { name: "apple", address: "Pune", validity: "3 years" },
-          { name: "banana", address: "Bnagalore", validity: "2 years" },
-          { name: "hello", address: "Delhi", validity: "120 years" },
-          { name: "apple", address: "Pune", validity: "3 years" },
-          { name: "banana", address: "Bnagalore", validity: "2 years" },
-          { name: "hello", address: "Delhi", validity: "120 years" },
-        ],
-      },
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+        };
+
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
     };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-  tick = () => {
-    this.setState({ date: new Date() });
-  };
-  stopTimer = () => {
-    clearInterval(this.timerID);
-  };
->>>>>>> b5080a2b2fd45448f2325df7aec04dd7ce681bad
+    tick = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+    stopTimer = () => {
+        clearInterval(this.timerID)
+    }
+    render() {
+        console.log(this.state.date.toLocaleTimeString)
+        return ( 
+            <div className = "App">
+                <h1> This is Keerthi Component</h1>
+                <h1> Hi Here is My Clock {this.state.date.toLocaleTimeString()} </h1> 
+                <h2> here my list </h2>
+                <My2ndComponent mydata = "thisnis my parent class" mydata1 = "hello" />
+                <Date2 />
+                loadDateCp = () => {
+                    this.setState({
+                        load: true
+                    })
+                }
+                unLoadDateCp = () => {
+                    this.setState({
+                        load: false
+                        })
+                    } 
+                    </div>
+                render() {
+                    console.log(this.state.date.toLocaleTimeString)
+                        return ( 
+                            <div className = "App">
+                            <h1> This is Keerthi Component </h1>
 
-  loadDateCp = () => {
-    this.setState({ load: true });
-  };
-  unLoadDateCp = () => {
-    this.setState({ load: false });
-  };
-  render() {
-    console.log(this.state.date.toLocaleTimeString);
+                                <h1> Hi Here is My Clock {this.state.date.toLocaleTimeString()} </h1> 
+                                    <h1> Here My List </h1> 
+                                    {this.state.listData.data.map((item) => (
+                                    <div>
+                                        <label>Name: <b>
+                                            {item.name} </b> 
+                                        </label> 
+                                        <label>Address: <b> 
+                                            {item.address} </b> 
+                                        </label> 
+                                        <label >
+                                            Validity: <b> 
+                                            {item.validity} </b>
+                                        </label>
+                            </div>
+                        ) 
+                }
+                /* {this.state.listData.map((item) => (
+                          <ul>
+                            <li>{item.name}</li>
+                          </ul>
+                ))} */}
 
-    return (
-      <div className="App">
-        <h1>This is Keerthi Component</h1>
+                        <My2ndComponent mydata = "Hiiiiii"hehe = "kjsfhsfkdghsdfkghlk" mydata1 = "Yashhhhhh" / >
+                        <button onClick = {this.loadDateCp}> Load Date2 Component </button> 
+                        <button onClick = {this.unLoadDateCp}> UNLoad Date2 Component </button>
+         
+                            {this.state.load ? < Date2 /> : null}
 
-        <h1>Hi Here is My Clock {this.state.date.toLocaleTimeString()}</h1>
-        <h1>Here My List</h1>
-        {this.state.listData.data.map((item) => (
-          <div>
-            <label>
-              Name: <b>{item.name} </b>
-            </label>
-            <label>
-              Address: <b>{item.address}</b>
-            </label>
-            <label>
-              Validity: <b>{item.validity}</b>
-            </label>
-          </div>
-        ))}
-        {/* {this.state.listData.map((item) => (
-          <ul>
-            <li>{item.name}</li>
-          </ul>
-        ))} */}
-        <My2ndComponent mydata="Hiiiiii" hehe="kjsfhsfkdghsdfkghlk" mydata1="Yashhhhhh" />
-        <button onClick={this.loadDateCp}>Load Date2 Component</button>
-        <button onClick={this.unLoadDateCp}>UNLoad Date2 Component</button>
-
-        {this.state.load ? <Date2 /> : null}
-
-        <button onClick={this.stopTimer}>Stop Timer</button>
-      </div>
-    );
-  }
-}
-
+                                <button onClick = {this.stopTimer}> Stop Timer </button> 
+             </div>
+            );     
+              
+        }
+    }
 export default Keerthi;
